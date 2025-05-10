2b2b7e1cd96394ccbf4af701075db58540e52927
hjj0106 <skyzzang0106@naver.com>
2025-05-10 21:28:53 +0900

    오타 조금 수정

--- apps/api/src/modules/auth/auth.service.ts
+++ apps/api/src/modules/auth/auth.service.ts
@@ -1,53 +1,49 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, DataSource } from 'typeorm';
-import { User } from '@modules/user/model/user.entity';
-import { LoginHistory } from '@modules/login_history/model/login_history.entity';
+import { DataSource } from 'typeorm';
 import { Request } from 'express';
 import { UserService } from '@modules/user/user.service';
 import { LoginHistoryService } from '@modules/login_history/login_history.service';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    @Inject(UserService) private readonly userService: UserService,
-    @Inject(LoginHistoryService) private readonly loginHistory: LoginHistoryService,
-    private readonly dataSource: DataSource,
+    constructor(
+		@Inject(UserService) private readonly userService: UserService,
+		@Inject(LoginHistoryService) private readonly loginHistory: LoginHistoryService,
+		private readonly dataSource: DataSource,
 
-    ) {}
+      ) {}
 
-  async validateAndLogUser(email: string, password: string, req: Request) {
-    const queryRunner = this.dataSource.createQueryRunner();
-    await queryRunner.connect();
-    await queryRunner.startTransaction();
+    async validateAndLogUser(email: string, password: string, req: Request) {
+		const queryRunner = this.dataSource.createQueryRunner();
+		await queryRunner.connect();
+		await queryRunner.startTransaction();
 
-    try {
-      const user = await this.userService.findOne({ where: { email } }, queryRunner);
+      	try {
+			const user = await this.userService.findOne({ where: { email } }, queryRunner);
 
-      if (!user || user.password !== password) {
-        throw new Error('Invalid credentials');
-      }
+			if (!user || user.password !== password) {
+				throw new Error('Invalid credentials');
+        	}
 
-      const history = this.loginHistory.create({
-        user: user,
-        userAgent: req.headers['user-agent'] || '',
-        browserName: 'Chrome', // 필요 시 파싱
-        osName: 'Windows',
-        osVersion: '10',
-        ipAddress: req.ip || '',
-        isLogin: true,
-      });
+			const history = this.loginHistory.create({
+				user: user,
+				userAgent: req.headers['user-agent'] || '',
+				browserName: 'Chrome', // 필요 시 파싱
+				osName: 'Windows',
+				osVersion: '10',
+				ipAddress: req.ip || '',
+				isLogin: true,
+			});
 
-      await queryRunner.manager.save(history);
-      await queryRunner.commitTransaction();
+			await queryRunner.manager.save(history);
+			await queryRunner.commitTransaction();
 
-      return { success: true, userId: user.id };
-    } catch (e) {
-      await queryRunner.rollbackTransaction();
-      return { success: false}
-    //   return { success: false, message: e?.message || '' };
-    } finally {
-      await queryRunner.release();
+			return { success: true, userId: user.id };
+		} catch (e: any) {
+			await queryRunner.rollbackTransaction();
+		  	return { success: false, message: e?.message || '' };
+		} finally {
+			await queryRunner.release();
+		}
     }
-  }
 }
